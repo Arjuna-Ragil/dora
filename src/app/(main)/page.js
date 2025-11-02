004349299005a3350a2475ba132a717eead9cdcc
HazriAE <hazriagung@gmail.com>
2025-11-02 15:39:23 +0700

    user profile v1

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,7 +1,9 @@
+import UserProfilePage from "../user_profile/UserProfile";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      test
+      <UserProfilePage />
     </div>
   );
 }
