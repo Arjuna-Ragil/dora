889bee04b13cefc7577f6275a93603b3e1f38c64
Arjuna <arjunaragilputera@gmail.com>
2025-11-02 18:04:59 +0700

    feat: login done

--- src/app/(main)/component/favorit.js
+++ src/app/(main)/component/favorit.js
@@ -1,4 +1,11 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
+
 export default function Favorit(){
+    const router = useRouter();
+
     return(
         <div className="min-h-screen h-full w-full md:grid grid-cols-2 flex items-center justify-center p-15" id="favorit">
             <div className="h-fit w-full flex flex-col self-center p-3 gap-10 items-center justify-center md:bg-gray-400/20 bg-gray-200/70 border backdrop-blur-md rounded-3xl animate-appear [animation-timeline:view()]">
@@ -7,7 +14,7 @@ export default function Favorit(){
                     <p>Kamu ingin makan makanan enak kemaren yaa??</p>
                     <p>Lihat daftar UMKM dan menu yang sudah kamu tandai sebagai favorit.</p>
                 </div>
-                <button className="bg-primary/20 hover:bg-primary/50 duration-150 border p-2 px-5 rounded-2xl">To Favorite</button>
+                <button onClick={() => router.push("/favorit")} className="bg-primary/20 hover:bg-primary/50 duration-150 border p-2 px-5 rounded-2xl">To Favorite</button>
             </div>
         </div>
     )
