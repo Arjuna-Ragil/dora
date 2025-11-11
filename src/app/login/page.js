fce94438a9e7a0f3c51a16968d8be989aed8daad
Arjuna <arjunaragilputera@gmail.com>
2025-11-11 08:56:11 +0700

    feat: team 1/2

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,8 +3,8 @@ import LiquidChrome from "./component/sideContent";
 
 export default function Login(){
     return(
-        <div className="min-h-screen h-full w-full grid grid-cols-2 gap-10 p-3">
-            <div className="h-full w-full p-3 rounded-2xl bg-black">
+        <div className="min-h-screen h-full w-full flex items-center justify-center md:grid grid-cols-2 gap-10 p-3">
+            <div className="h-full w-full p-3 rounded-2xl bg-black max-md:hidden">
                 <LiquidChrome
                     baseColor={[0, 0, 0.1]}
                     speed={0.5}
