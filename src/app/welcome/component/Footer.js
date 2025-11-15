fc3c644b3c6df6b3eb3e5b1ed81eca2eee135b67
Arjuna <arjunaragilputera@gmail.com>
2025-11-15 11:44:25 +0700

    feat: team done

--- src/app/welcome/component/Footer.js
+++ src/app/welcome/component/Footer.js
@@ -1,38 +1,31 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function Footer(){
   return (
     <div className='w-full flex flex-col  md:pt-10 px-0 pt-10 pb-2 bg-card rounded-2xl items-center justify-center md:gap-15 gap-8'>
-        <div className='w-full flex flex-row lg:px-15 px-5 sm:px-9 md:px-12 items-center justify-between max-md:gap-5'>
-            <div className='w-full flex flex-col items-start gap-3'>
-                    <img src="/logo light.svg" 
-                    alt="Planix Logo"
-                    className='size-15 2xl:size-20 block' />
-                    <h2 className='text-xl md:text-5xl mt-2  font-bold bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent'>Planix</h2>
-                <p className='text-xs lg:text-2xl md:text-lg text-left font-medium text-text'>Smarter planning, powered by people & data</p>
+        <div className='w-full grid grid-cols-3 lg:px-15 px-5 sm:px-9 md:px-12 items-center justify-between max-md:gap-5'>
+            <div className='w-full flex flex-col items-start gap-3 col-span-2'>
+                    <div className='h-30 w-auto relative aspect-square'>
+                        <Image src="/doraLogo.svg" alt="Planix Logo" fill/>
+                    </div>
+                    <h2 className='text-xl md:text-5xl mt-2  font-bold bg-linear-to-r from-primary to-secondary bg-clip-text text-transparent'>Dora</h2>
+                <p className='text-xs lg:text-2xl md:text-lg text-left font-medium text-text'>Dukung UMKM Lokal, Mulai dari Sekitar Kita</p>
             </div>
 
-            <div className='w-full flex flex-row gap-3 sm:gap-5 md:gap-15 lg:gap-20 sm:justify-end justify-center'>
-                <div className='flex flex-col justify-start sm:gap-2 md:gap-3 gap-1 text-text'>
-                    <h3 className='md:text-xl sm:text-lg text-base font-semibold '>Product</h3>
+            <div className='flex flex-col gap-3  justify-self-end'>
+                    <h3 className='md:text-xl sm:text-lg text-base font-semibold '>Quick Navigation</h3>
+                    <a href='/welcome#hero' 
+                    className='hover:text-hover-button duration-150 text-small-font sm:text-xs md:text-base'>Home</a>
                     <a href='/welcome#features' 
                     className='hover:text-hover-button duration-150 text-small-font sm:text-xs md:text-base'>Features</a>
                     <a href='/welcome#demo' 
                     className='hover:text-hover-button duration-150 text-small-font sm:text-xs md:text-base'>Demo</a>
-                    <a href='/welcome#flow' 
-                    className='hover:text-hover-button duration-150 text-small-font sm:text-xs md:text-base'>How it Works</a>
-                </div>
-                <div className='flex flex-col gap-2 text-text'>
-                    <h3 className='md:text-xl sm:text-lg text-base font-semibold '>Team</h3>
-                    <a href='/welcome#team' 
-                    className='hover:text-hover-button duration-150 text-small-font sm:text-xs md:text-base'>Our Team</a>
-                </div>
             </div>
         </div>
 
-        <div className='w-full space-y-2 pb-1'>
-            <div className='w-full md:h-1 h-0.5 bg-footer'></div>
-            <p className='text-center md:text-lg sm:text-xs text-[10px] text-text'>© 2025 Shadow Army. All rights reserved.</p>
+        <div className='w-full space-y-2 pb-1 border-t'>
+            <p className='text-center md:text-lg sm:text-xs text-[10px] text-text p-2'>© 2025 Shadow Army. All rights reserved.</p>
         </div>
     </div>
   )
